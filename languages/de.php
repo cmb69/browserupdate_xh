7f48e230f8013ecff7b7a78eae252b202f6d6723
cmb69 <cmbecker69@gmx.de>
2014-06-22 18:33:34 +0000

    added German localization

---
+++ languages/de.php
@@ -0,0 +1,13 @@
+<?php
+
+$plugin_tx['browserupdate']['alt_icon']="Update";
+$plugin_tx['browserupdate']['cf_version_explorer']="Die neueste Version von Microsoft Internet Explorer, die als veraltet gemeldet werden soll. Leer lassen, um einen sinnvollen Vorgabewert zu verwenden.";
+$plugin_tx['browserupdate']['cf_version_firefox']="Die neueste Version von Mozilla Firefox, die als veraltet gemeldet werden soll. Leer lassen, um einen sinnvollen Vorgabewert zu verwenden.";
+$plugin_tx['browserupdate']['cf_version_opera']="Die neueste Version von Opera, die als veraltet gemeldet werden soll. Leer lassen, um einen sinnvollen Vorgabewert zu verwenden.";
+$plugin_tx['browserupdate']['cf_version_safari']="Die neueste Version von Apple Safari, die als veraltet gemeldet werden soll. Leer lassen, um einen sinnvollen Vorgabewert zu verwenden.";
+$plugin_tx['browserupdate']['cf_version_chrome']="Die neueste Version von Google Chrome, die als veraltet gemeldet werden soll. Leer lassen, um einen sinnvollen Vorgabewert zu verwenden.";
+$plugin_tx['browserupdate']['cf_reminder']="Die Anzahl von Stunden nach denen die Meldung wieder angezeigt werden soll (bei \"0\" wird sie immer angezeigt).";
+$plugin_tx['browserupdate']['cf_cms_language']="Ob die CMSimple-Sprache für den Meldungstext verwendet werden soll (im Gegensatz zu den Browsereinstellungen des Besuchers).";
+$plugin_tx['browserupdate']['cf_test']="Ob der Test-Modus aktiviert werden soll (d.h. die Update-Meldung wird immer angezeigt).";
+
+?>
