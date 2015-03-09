28e56fb23c9a488986f815f81a7c077be053b1e9
Christoph M. Becker <cmbecker69@gmx.de>
2015-03-09 23:18:56 +0100

    bumped copyright year

--- classes/Presentation.php
+++ classes/Presentation.php
@@ -8,7 +8,7 @@
  * @category  CMSimple_XH
  * @package   Browserupdate
  * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2014 Christoph M. Becker <http://3-magi.net>
+ * @copyright 2014-2015 Christoph M. Becker <http://3-magi.net>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link      http://3-magi.net/?CMSimple_XH/Browserupdate_XH
  */
@@ -205,7 +205,7 @@ EOT;
     private function _renderCopyright()
     {
         return <<<EOT
-<p>Copyright &copy; 2014
+<p>Copyright &copy; 2014-2015
     <a href="http://3-magi.net/" target="_blank">Christoph M. Becker</a>
 </p>
 EOT;
