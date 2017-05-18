462454a6b69a4902a3b3a925a03e2244dc4bfce8
Christoph M. Becker <cmbecker69@gmx.de>
2017-05-18 19:10:31 +0200

    Bump copyright year

--- classes/Controller.php
+++ classes/Controller.php
@@ -8,7 +8,7 @@
  * @category  CMSimple_XH
  * @package   Browserupdate
  * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2014-2015 Christoph M. Becker <http://3-magi.net>
+ * @copyright 2014-2017 Christoph M. Becker <http://3-magi.net>
  * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link      http://3-magi.net/?CMSimple_XH/Browserupdate_XH
  */
@@ -219,7 +219,7 @@ EOT;
     protected function renderCopyright()
     {
         return <<<EOT
-<p>Copyright &copy; 2014-2015
+<p>Copyright &copy; 2014-2017
     <a href="http://3-magi.net/" target="_blank">Christoph M. Becker</a>
 </p>
 EOT;
